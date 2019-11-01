23d6666ac4efad2c1472fc2b2a85df0bbcb5f137
LiamPKenna <liampkenna@gmail.com>
2019-11-01 08:17:51 -0700

    add scripts.js file with basic UI setup

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// GLOBAL VARIABLES
+
+// TEMPLATING
+
+// MAIN LOGIC
+
+// UI
+$(document).ready(function() {
+
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    $('#results').append('');
+  });
+
+});
+
+
+/*
+JavaScript business logic and user interface logic are separate.
+
+Logic is broken down into "plain English" specs, listed in README.
+
+Required functionality is in place by Friday deadline.
+
+Project demonstrates understanding of this week's concepts. If prompted, you are able to discuss your code with an instructor using correct terminology.
+
+Application implements a loop and works as expected.
+
+All previous standards are in place (see below).
+
+The user can use the app repeatedly and see new results.
+*/
